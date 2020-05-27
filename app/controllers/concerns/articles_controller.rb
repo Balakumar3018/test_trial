9009f45fd3693a70b859f7e7b44961ab08086d34
Balakumar3018 <balakumarjkp@gmail.com>
2020-05-27 15:42:41 +0000

    (article saves and redirect)

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -1,15 +1,22 @@
 class ArticlesController < ApplicationController
    def show
-      @article=Article.find(params[:id])
+    @article=Article.find(params[:id])
    end 
    def index
-      @articles=Article.all
+    @articles=Article.all
    end
    def new 
-      
+    @article=Article.new 
    end 
    def create 
-      render plain:params[:article]
+      #render plain:params[:article]
+    @article=Article.new(params.require(:article).permit(:title, :description))
+     # render plain:@article.inspect
+    if   @article.save
+     flash[:notice]="Article was suucessfully created "
+     redirect_to @article
+   else
+    render 'new'
    end 
- 
+ end
 end
