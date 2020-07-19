01707a70ae9491c4f63a0f74c2cfbda7a339706a
Balakumar <balakumarjkp@gmail.com>
2020-07-19 16:13:25 +0000

    create new users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+    def new
+       @user=User.new 
+    end
+    def create
+       @user=User.new(user_params)
+       if @user.save
+           flash[:notice]="Welcome to test trial #{@user.username}, and you have logged in successfully"
+           redirect_to articles_path
+       else
+           render 'new'
+       end
+   end
+   def  user_params
+    params.require(:user).permit(:username, :email, :password)
+end
+end    
